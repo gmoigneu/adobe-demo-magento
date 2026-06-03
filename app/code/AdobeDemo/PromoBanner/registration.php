38ba45b7c665885aa4c04c7314105e84f2db1251
Obvious Agent <obvious-agent@upsun.com>
2026-06-03 16:11:22 +0200

    obvious: implement plan for #2

---
+++ app/code/AdobeDemo/PromoBanner/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Free-shipping promo banner module registration.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'AdobeDemo_PromoBanner',
+    __DIR__
+);
