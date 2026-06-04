4ccc5a15cf800f3e2c7b91a6d987f9a6b79d8583
Dispatch Agent <dispatch-agent@upsun.com>
2026-06-04 08:07:16 +0000

    dispatch: implement plan for #4

---
+++ app/code/Demo/PromoBanner/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Demo_PromoBanner component registration
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Demo_PromoBanner',
+    __DIR__
+);
