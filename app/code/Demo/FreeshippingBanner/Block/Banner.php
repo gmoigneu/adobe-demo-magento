43011fa45bc073569a9b1292e12e49330c034adb
Dispatch Agent <dispatch-agent@upsun.com>
2026-06-05 13:53:12 +0000

    dispatch: implement plan for #8

---
+++ app/code/Demo/FreeshippingBanner/Block/Banner.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Free Shipping Banner Block
+ */
+namespace Demo\FreeshippingBanner\Block;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Banner block for the free shipping promotional strip.
+ */
+class Banner extends Template
+{
+    /** @var string */
+    protected $_template = 'Demo_FreeshippingBanner::banner.phtml';
+}
---
+++ app/code/Demo/FreeshippingBanner/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Free Shipping Banner module registration.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Demo_FreeshippingBanner',
+    __DIR__
+);
